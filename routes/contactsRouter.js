4e28c88dcfc6062031604e84bd732945ece77ea5
danadobr3 <134941956+danadobr3@users.noreply.github.com>
2024-03-05 15:46:42 +0100

    middlewares

--- routes/contactsRouter.js
+++ routes/contactsRouter.js
@@ -12,22 +12,25 @@ import validateBody from "../helpers/validateBody.js";
 import {
   createContactSchema,
   updateContactSchema,
+  updateStatusSchema
 } from "../schemas/contactsSchemas.js";
 
 import { contacts } from "../models/contacts.js";
+
+import isValidId from "../helpers/isValidId.js";
 
 const contactsRouter = express.Router();
 
 contactsRouter.get("/", getAllContacts);
 
-contactsRouter.get("/:id", getOneContact);
+contactsRouter.get("/:id", isValidId, getOneContact);
 
-contactsRouter.delete("/:id", deleteContact);
+contactsRouter.delete("/:id", isValidId, deleteContact);
 
 contactsRouter.post("/", validateBody(createContactSchema), createContact);
 
-contactsRouter.put("/:id", validateBody(updateContactSchema), updateContact);
+contactsRouter.put("/:id", isValidId, validateBody(updateContactSchema), updateContact);
 
-contactsRouter.patch("/:id/favorite", updateStatusContact);
+contactsRouter.patch("/:id/favorite", isValidId, validateBody(updateStatusSchema), updateStatusContact);
 
 export default contactsRouter;
